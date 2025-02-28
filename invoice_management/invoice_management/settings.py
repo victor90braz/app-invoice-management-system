febfa8e680c7cc7486b40a4dc9b9947a4071b75e
dev-victor-braz <victor.90braz@gmail.com>
2025-02-28 12:03:43 +0100

    set up database

--- invoice_management/invoice_management/settings.py
+++ invoice_management/invoice_management/settings.py
@@ -66,9 +66,13 @@ WSGI_APPLICATION = 'invoice_management.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "invoice_management_db",
+        "USER": "postgres",
+        "PASSWORD": "root",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
